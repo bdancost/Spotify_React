1e102a5748de8dc32b5c44a3eaf8f04988808b21
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-11 20:07:46 -0300

    Configurando Estrutura do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logoSpotify from "../assets/logo/spotify-logo.png";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logoSpotify} alt="Logo do Spotify" />
+      <a href="/">
+        <h1>Spotify</h1>
+      </a>
+    </div>
+  );
+};
+
+export default Header;
