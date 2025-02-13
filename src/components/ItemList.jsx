1383877910e846378676ff29f359ddcbc5132c55
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-13 15:57:13 -0300

    Preparando os components da Page

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,15 +1,23 @@
 import SingleItem from "./SingleItem";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <Link to={path} className="item-list__link">
-          Mostrar tudo
-        </Link>
+
+        {isHome ? (
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
 
       <div className="item-list__container">
