c0ed1c6475b227330e42489e5fe32bd198c2f3c3
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-17 19:57:35 -0300

    Ajustes no progress env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3000/api";
+const URL = "https://spotify-react-6thd.onrender.com/api";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
