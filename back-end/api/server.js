35538e70774cd1621f30366a9e8ef0d3016fe81c
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-17 06:25:47 -0300

    Ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -3,6 +3,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -19,6 +22,12 @@ app.get("/api/artists", async (req, res) => {
 
 app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
